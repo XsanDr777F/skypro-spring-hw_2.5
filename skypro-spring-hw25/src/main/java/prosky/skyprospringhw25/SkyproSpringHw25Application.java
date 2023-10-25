09f744d62460718cb1980058ba071407736fc3e2
Александр <79691189430@yandex.ru>
2023-10-25 16:57:41 +0300

    Initial commit

---
+++ skypro-spring-hw25/src/main/java/prosky/skyprospringhw25/SkyproSpringHw25Application.java
@@ -0,0 +1,13 @@
+package prosky.skyprospringhw25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHw25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringHw25Application.class, args);
+	}
+
+}
---
+++ skypro-spring-hw25/src/test/java/prosky/skyprospringhw25/SkyproSpringHw25ApplicationTests.java
@@ -0,0 +1,13 @@
+package prosky.skyprospringhw25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringHw25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
